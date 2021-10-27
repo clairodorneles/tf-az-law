a9f0b87a99b7856780c6b3f9282e4236a0d935fd
Clairo <clairodorneles@gmail.com>
2021-10-27 17:48:36 -0300

    change output file

--- output.tf
+++ output.tf
@@ -6,7 +6,6 @@ output "law_workspace_id" {
     value = azurerm_log_analytics_workspace.law.workspace_id
 }
 
-
 output "law_psk_01" {
     value = azurerm_log_analytics_workspace.law.primary_shared_key
 }
